1d2560b116fc7214e6338e5337e512aa494eb52a
Cris Peraferrer Mayné <Cristian.Peraferrer@sunwebgroup.com>
2023-06-21 16:58:35 +0200

    critical damage

--- csharp/Game.tests/AttackCalculatorTests.cs
+++ csharp/Game.tests/AttackCalculatorTests.cs
@@ -7,9 +7,8 @@ namespace Game.tests
 {
     public class AttackCalculatorTests 
     {
-        // 1. Attacker doesn't damage defender
-        // 2. Attacker damages defender
-        // 3. Attacker misses
+        
+        // 4. Attacker does critical damage
 
         [Fact]
         public void AttackerDoesNotDamageDefender()
@@ -27,13 +26,42 @@ namespace Game.tests
         public void AttackerDamagesDefender()
         {
             var uselessNumber = 0;
-            var attacker = new Character(uselessNumber, uselessNumber, "A race", 1);
+            var damage = 1;
+            var attacker = new Character(uselessNumber, damage, "A race", 1);
             var defender = new Character(0, uselessNumber, "A race", uselessNumber);
             var sut = new AttackCalculatorTestable(2);
 
             var result = sut.CalculateDamage(attacker, defender);
 
-            Assert.Equal(-1, result);
+            Assert.Equal(damage, result);
+        }
+
+        [Fact]
+        public void AttackerMisses()
+        {
+            var uselessNumber = 0;
+            var damage = 1;
+            var attacker = new Character(uselessNumber, damage, "A race", 1);
+            var defender = new Character(0, uselessNumber, "A race", uselessNumber);
+            var sut = new AttackCalculatorTestable(1);
+
+            var result = sut.CalculateDamage(attacker, defender);
+
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void AttackerInflictsCriticalDamage()
+        {
+            var uselessNumber = 0;
+            var damage = 1;
+            var attacker = new Character(uselessNumber, damage, "A race", 1);
+            var defender = new Character(0, uselessNumber, "A race", uselessNumber);
+            var sut = new AttackCalculatorTestable(20);
+
+            var result = sut.CalculateDamage(attacker, defender);
+
+            Assert.Equal(2, result);
         }
     }
 
